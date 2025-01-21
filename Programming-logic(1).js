33b1d7c651f392aedff2c37da657656ffb2d083f
Juan Alberto Santiago De Vicente <97377162+JuanAlbertoSantiago@users.noreply.github.com>
2025-01-21 15:58:57 -0600

    Solutions to the challenges
    I added all the solutions to the challenges I had in class today.

--- Programming-logic(1).js
+++ Programming-logic(1).js
@@ -4,5 +4,30 @@ let diaDeLaSemana = prompt("Puedes indicar qué día de la semana es hoy:");
 if (diaDeLaSemana == "Sábado" || diaDeLaSemana == "Domingo"){
     alert("¡Buen fin de semana!");
 } else {
-    alert("¡Buena Semana!")
+    alert("¡Buena Semana!");
 }
+
+// 2. Verifica si un número ingresado por el usuario es positivo o negativo. Muestra una alerta informativa.
+let numeroDelUsuario = prompt("Ingresa un número positivo o negativo:");
+
+if (numeroDelUsuario > 0){
+    alert("El número que ingresaste fue positivo");
+} else if (numeroDelUsuario < 0){
+    alert("El número que ingresaste fue negativo");
+} else {
+    alert("El número que ingresaste fue cero")
+}
+
+// 3. Crea un sistema de puntuación para un juego. Si la puntuación es mayor o igual a 100, muestra "¡Felicidades, has ganado!". En caso contrario, muestra "Intentalo nuevamente para ganar.".
+let puntuacion = 200; //Suponemos que sabemos la puntuación
+
+if (puntuacion >= 100){
+    console.log("¡Felicidades, has ganado!");
+} else {
+    console.log("Intentalo nuevamente para ganar");
+}
+
+// 4. Crea un mensaje que informe al usuario sobre el saldo de su cuenta, utilizando un template string para incluir el valor del saldo.
+let saldoCuenta = 200; //Ejemplo de Saldo
+
+alert(`Tu saldo es de ${saldoCuenta}.`);
